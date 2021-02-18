ef7460893f9cc29c6c331daf639e6e0ee1ab7437
Rashid Ahmad <decent10cs@gmail.com>
2021-02-18 12:42:39 +0300

    Added screens for login, home + implement login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+ import {
+   QueryClient,
+   QueryClientProvider,
+ } from 'react-query'
+ 
+
+import { AuthProvider } from  "./contexts/AuthContext";
+import PrivateRoute from "./components/RequireAuth";
+import Home from "./screens/Home";
+import Orders from "./screens/Orders";
+import Login from "./screens/Login";
+
+ // Create a client
+ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <PrivateRoute exact path="/" component={Home} />
+   
+          <Route path="/orders">
+            <Orders />
+          </Route>
+           <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+      </AuthProvider>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
